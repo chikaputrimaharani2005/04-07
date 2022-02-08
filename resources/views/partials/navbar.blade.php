828f238f434859271e1cffbe42d2ba8737f1dd12
Your Name <you@example.com>
2022-02-08 08:07:44 +0700

    upload modul 3

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
         <div class="container">
-            <a class="navbar-brand" href="/">ini saya</a>
+            <a class="navbar-brand" href="/">Chika</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -12,8 +12,11 @@
         <li class="nav-item">
         <a class="nav-link {{ ( $title === 'About') ? 'active' : ''}}" href="/about">About</a>
         </li>
-        <li class="nav-item dropdown">
+        <li class="nav-item">
         <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : ''}}" href="/gallery">Gallery</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/contacts">Contacts Us</a> 
         </li>
       
     </ul>
